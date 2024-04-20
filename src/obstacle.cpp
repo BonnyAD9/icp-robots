7602592d421abba5f8b6f82d8a796db1f843f719
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-04-20 15:48:14 +0200

    Add resizing of obstacles

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,17 +1,120 @@
 #include "obstacle.hpp"
 
+#include <iostream>
+
 #include <QBrush>
 #include <QPen>
 #include <QGraphicsSceneMouseEvent>
 
 namespace icp {
 
+Obstacle::State operator|=(Obstacle::State &l, const Obstacle::State &r) {
+    return l = static_cast<Obstacle::State>(
+        static_cast<int>(l) | static_cast<int>(r)
+    );
+}
+
+Obstacle::State operator^=(Obstacle::State &l, const Obstacle::State &r) {
+    return l = static_cast<Obstacle::State>(
+        static_cast<int>(l) ^ static_cast<int>(r)
+    );
+}
+
+Obstacle::State operator&(const Obstacle::State &l, const Obstacle::State &r) {
+    return static_cast<Obstacle::State>(
+        static_cast<int>(l) & static_cast<int>(r)
+    );
+}
+
+Obstacle::State operator|(const Obstacle::State &l, const Obstacle::State &r) {
+    return static_cast<Obstacle::State>(
+        static_cast<int>(l) | static_cast<int>(r)
+    );
+}
+
+bool operator*(const Obstacle::State &l, const Obstacle::State &r) {
+    return (l & r) == r;
+}
+
+Obstacle::State operator~(const Obstacle::State &s) {
+    return static_cast<Obstacle::State>(~static_cast<int>(s));
+}
+
 Obstacle::Obstacle(QRectF hitbox, QGraphicsItem *parent)
     : QGraphicsRectItem(hitbox, parent)
 {
     setBrush(QBrush(QColor(0xff, 0x55, 0x55)));
-    setPen(QPen(QColor(0xff, 0xff, 0xff)));
+    setPen(QPen(
+        QColor(0xff, 0xff, 0xff),
+        5,
+        Qt::SolidLine,
+        Qt::SquareCap,
+        Qt::PenJoinStyle::SvgMiterJoin
+    ));
     setFlag(ItemIsMovable);
+}
+
+void Obstacle::mousePressEvent(QGraphicsSceneMouseEvent *event) {
+    constexpr qreal R_BORDER = 2.5;
+
+    state = State::None;
+
+    auto relPos = event->scenePos() - rect().topLeft();
+
+    if (relPos.x() <= R_BORDER) {
+        state |= State::ResizeHorizontal | State::ResizeLeft;
+    } else if (rect().width() - relPos.x() <= R_BORDER) {
+        state |= State::ResizeHorizontal;
+    }
+
+    if (relPos.y() <= R_BORDER) {
+        state |= State::ResizeVertical | State::ResizeTop;
+    } else if (rect().height() - relPos.y() <= R_BORDER) {
+        state |= State::ResizeVertical;
+    }
+
+    if (state == State::None) {
+        state = State::Dragging;
+    }
+    grabMouse();
+}
+
+void Obstacle::mouseReleaseEvent(QGraphicsSceneMouseEvent *event) {
+    state = State::None;
+    ungrabMouse();
+}
+
+void Obstacle::mouseMoveEvent(QGraphicsSceneMouseEvent *event) {
+    auto delta = event->scenePos() - event->lastScenePos();
+    QRectF rec = rect();
+
+    if (state == State::Dragging) {
+        rec.moveTopLeft(rec.topLeft() + delta);
+    }
+
+    if (state * State::ResizeHorizontal) {
+        if (state * State::ResizeLeft) {
+            rec.setLeft(rec.left() + delta.x());
+        } else {
+            rec.setRight(rec.right() + delta.x());
+        }
+        if (rec.width() < 0) {
+            state ^= State::ResizeLeft;
+        }
+    }
+
+    if (state * State::ResizeVertical) {
+        if (state * State::ResizeTop) {
+            rec.setTop(rec.top() + delta.y());
+        } else {
+            rec.setBottom(rec.bottom() + delta.y());
+        }
+        if (rec.height() < 0) {
+            state ^= State::ResizeTop;
+        }
+    }
+
+    setRect(rec.normalized());
 }
 
 }
--- src/obstacle.hpp
+++ src/obstacle.hpp
@@ -8,6 +8,23 @@ namespace icp {
 class Obstacle : public QGraphicsRectItem {
 public:
     explicit Obstacle(QRectF hitbox, QGraphicsItem *parent = nullptr);
+
+    enum class State : int {
+        None = 0x0,
+        Dragging = 0x1,
+        ResizeHorizontal = 0x2,
+        ResizeVertical = 0x4,
+        ResizeLeft = 0x8,
+        ResizeTop = 0x10,
+    };
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
+
+private:
+    State state;
 };
 
 } // namespace icp
