a30dc84626dd563cac1c18425f83960172634025
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-04-20 14:18:36 +0200

    Simpler way to implement moving :)

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -7,25 +7,11 @@
 namespace icp {
 
 Obstacle::Obstacle(QRectF hitbox, QGraphicsItem *parent)
-    : QGraphicsRectItem(hitbox, parent), dragging(false)
+    : QGraphicsRectItem(hitbox, parent)
 {
     setBrush(QBrush(QColor(0xff, 0x55, 0x55)));
     setPen(QPen(QColor(0xff, 0xff, 0xff)));
-}
-
-void Obstacle::mousePressEvent(QGraphicsSceneMouseEvent *event) {
-    grabMouse();
-    dragging = true;
-}
-
-void Obstacle::mouseReleaseEvent(QGraphicsSceneMouseEvent *event) {
-    ungrabMouse();
-    dragging = false;
-}
-
-void Obstacle::mouseMoveEvent(QGraphicsSceneMouseEvent *event) {
-    auto delta = event->scenePos() - event->lastScenePos();
-    moveBy(delta.x(), delta.y());
+    setFlag(ItemIsMovable);
 }
 
 }
